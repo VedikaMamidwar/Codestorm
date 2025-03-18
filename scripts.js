ae0978853471276d8bd107592e733f707adf9c08
VedikaMamidwar <vedikamamidwar5@gmail.com>
2025-03-18 17:52:35 +0530

    initial commit

---
+++ scripts.js
@@ -0,0 +1,12 @@
+// Dark Mode Toggle
+const themeToggle = document.getElementById("theme-toggle");
+
+themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+    
+    if (document.body.classList.contains("dark-mode")) {
+        themeToggle.textContent = "☀️ Light Mode";
+    } else {
+        themeToggle.textContent = "🌙 Dark Mode";
+    }
+});
